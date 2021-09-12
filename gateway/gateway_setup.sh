28d27228d9df4425fd6a431235b23d69668bcfd1
wiktorkisielewski <wiktor.kisielewski@protonmail.com>
2021-09-12 17:51:51 +0200

    0.2

--- gateway/gateway_setup.sh
+++ gateway/gateway_setup.sh
@@ -6,5 +6,5 @@ wget -O gateway.zip https://download2.interactivebrokers.com/portal/clientportal
 && unzip gateway.zip -d gateway/ \
 && rm gateway.zip \
 && cd gateway \
-&& sed -i -e 's@deny:@  - 83.25.162.33\n      deny:@g' root/conf.yaml \
+&& sed -i -e 's@deny:@  - $(hostname -I | awk '{print $1}')\n  - $ALLOWED_IP\n      deny:@g' root/conf.yaml \
 && bin/run.sh root/conf.yaml
